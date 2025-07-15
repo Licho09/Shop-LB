d515633a95f1967c5d96b711ba02a304c857cc16
Luis Chirinos <luischirinos1000@gmail.com>
2025-07-15 15:29:36 -0500

    Fix: export default config for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-`import { defineConfig } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === "build" ? "/Shop-LB/" : "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
-}))
-`
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "build" ? "/Shop-LB/" : "/",
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+    },
+    optimizeDeps: {
+      exclude: ["lucide-react"],
+    },
+  }
+})
